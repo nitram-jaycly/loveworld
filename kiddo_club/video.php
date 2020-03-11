4ddc649fef402403ad9cd8e90ee95e1036898b8b
nitram-jaycly <37207389+nitram-jaycly@users.noreply.github.com>
2020-03-11 18:17:25 +0200

    Add files via upload

---
+++ kiddo_club/video.php
@@ -0,0 +1,79 @@
+<html><style type="text/css">
+<!--
+body {
+	background-color: #000000;
+	background-image: url(images/radio_pic.png);
+	background-repeat-x: no-repeat;
+	background-repeat-y: no-repeat;
+	
+}
+-->
+</style>
+<script type="text/javascript" src="/res/js/swfobject.js"></script>
+<script src="Scripts/AC_RunActiveContent.js" type="text/javascript"></script>
+<style type="text/css">
+<!--
+body {
+	margin-left: 0px;
+	margin-top: 0px;
+	margin-right: 0px;
+	margin-bottom: 0px;
+}
+-->
+</style>
+<body>
+<div align="center">
+<div id='preview' style="background:#000; padding:0px 10px;">If you are seeing this message, it means that you do not have JavaScript enabled in your browser therefore you would be unable
+to view the stream. Please use a Javascript enabled browser such as Internet Explorer 7 or Mozilla Firefox</div>
+  <script type='text/javascript'>
+   var s2 = new SWFObject('/res/flash/player2.swf','ply','500','330','9','#ffffff');
+  	s2.addParam("allowfullscreen","true");
+	s2.addParam("wmode","transparent");
+	s2.addVariable("autostart","true");
+	s2.addVariable("showimage","false");
+	s2.addVariable("repeat","list");
+	s2.addVariable('autoscroll','false'); 
+	s2.addVariable('shuffle','false'); 
+	s2.addParam('allowscriptaccess','always');
+	s2.addVariable('bufferlength','1');
+	s2.addVariable("enablejs","true");
+	s2.addVariable('javascriptid','plyr');
+	//rtmp://154.obj.netromedia.com/IMMPowered
+    
+
+	//s2.addVariable("streamer","rtmp://155.obj.netromedia.com/MobileStream");
+	//s2.addVariable('file','Churchaud');
+	
+	//s2.addVariable("streamer","rtmp://155.obj.netromedia.com/MobileStream");
+	s2.addVariable("streamer","rtmp://183.obj.netromedia.com/LoveWorldtv2");
+	s2.addVariable('file','lwsataudio1');
+	
+	//s2.addVariable("streamer","rtmp://154.obj.netromedia.com/LagosLCC1");
+        //s2.addVariable('file','GlobalclassB');
+
+	//s2.addVariable("rtmp.subscribe","true");
+	//s2.addVariable("displayheight","344");
+	s2.addVariable("backcolor","0x000000");
+	s2.addVariable("frontcolor","0xCCCCCC");
+	s2.addVariable("lightcolor","0x557722");
+	//s2.addVariable("width","430");
+	//s2.addVariable("height","344");
+	s2.addVariable('provider','rtmp'); 
+	
+	s2.addVariable("type","video");  
+	s2.addVariable('skin','/res/flash/stijl.swf'); 
+	//s2.addVariable("stretching","exactfit"); 
+  s2.write('preview');
+</script>
+<script type="text/javascript">
+var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+</script>
+<script type="text/javascript">
+try {
+var pageTracker = _gat._getTracker("UA-8378729-1");
+pageTracker._trackPageview();
+} catch(err) {}</script>
+</div>
+</body>
+</html>
